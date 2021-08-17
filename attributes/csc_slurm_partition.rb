8bd83d1d187a4c6f6b507d759fef67347b868e19
Robin Karlsson <robin.karlsson@csc.fi>
2021-08-17 14:12:08 +0300

    Add partition customization

--- attributes/csc_slurm_partition.rb
+++ attributes/csc_slurm_partition.rb
@@ -16,10 +16,27 @@ module SmartAttributes
         "select"
       end
 
+      # Filter the list of partitions based on the values provided in form.yml.erb
+      def filter_partitions(partitions)
+        if !opts[:ignore].nil?
+          # Filter out values provided in the ignore attribute on csc_slurm_partition
+          partitions.select {|p| !opts[:ignore].include?(p) }
+        elsif !opts[:select].nil?
+          # Only include values provided in the select attribute on csc_slurm_partition
+          partitions.select {|p| opts[:select].include?(p) }
+        else
+          partitions
+        end
+      end
+
       def get_partitions
-          sacct_res = `#{__dir__}/../scripts/p_and_p.sh`
+        # Allow providing a fixed list of partitions without getting from Slurm
+        if !opts[:partitions].nil?
+          return opts[:partitions]
+        end
+        sacct_res = `#{__dir__}/../scripts/p_and_p.sh`
         res_arr = sacct_res.split('@')
-        slurm_partitions = res_arr[0].split(' ')       
+        filter_partitions(res_arr[0].split(' '))
       end
 
       # Form label for this attribute
