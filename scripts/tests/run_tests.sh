e1c38f7b8457f6544c4785be5ae277775f64d9cb
Robin Karlsson <robin.karlsson@csc.fi>
2021-12-10 12:55:40 +0200

    Clean up Slurm limits script and add some tests

--- scripts/tests/run_tests.sh
+++ scripts/tests/run_tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# This scripts runs some tests for the scripts that fetch information from Slurm
+# basically only smoke testing
+
 ruby -Ilib:${SCRIPT_DIR}/../ ${SCRIPT_DIR}/test_slurm_project_partition.rb
+ruby -Ilib:${SCRIPT_DIR}/../ ${SCRIPT_DIR}/test_slurm_limits.rb
