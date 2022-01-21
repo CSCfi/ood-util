d3b5420c3182658a08859b2b65a415939f3dfe30
Robin Karlsson <robin.karlsson@csc.fi>
2022-01-21 12:17:37 +0200

    Fix OOD_BC_DYNAMIC_JS 2.0.22 changes

--- scripts/slurm_project_partition.rb
+++ scripts/slurm_project_partition.rb
@@ -105,12 +105,7 @@ module SlurmProjectPartition
       parts.transform_values do |projects|
         # select the project that are not allowed for this partition
         (all_projects - projects).map do |project|
-          # OOD has strange behaviour for the way these project names need to behave.
-          # Underscore needs to be replaced by dash if the following character is a letter,
-          # otherwise remove the underscore.
-          # ood_installation => ood-installation
-          # project_123456 => project123456
-          proj_name = project.gsub(/_([a-z])/, '-\1').gsub(/_/, '')
+          proj_name = project.gsub(/_/, '-')
           {"data-option-for-csc-slurm-project-#{proj_name}".to_sym => false}
         end
       end
