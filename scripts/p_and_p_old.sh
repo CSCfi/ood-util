f960740cba1a673e6f97c5329e5b9b391fd8a000
Robin Karlsson <robin.karlsson@csc.fi>
2022-08-05 10:40:08 +0300

    Remove unused scripts

--- scripts/p_and_p_old.sh
+++
@@ -1,6 +0,0 @@
-sact_res=$(sacctmgr -p show -n assoc where user=$USER | cut -d "|" -f 2,4)
-partitions=$(echo "$sact_res" | cut -d "|" -f 1 | sort | uniq |  grep -v "default_no_jobs" )
-projects=$(echo "$sact_res" | cut -d "|" -f 2 | sort | uniq | grep -v "^$")
-echo "$projects"
-echo "@"
-echo "$partitions"
