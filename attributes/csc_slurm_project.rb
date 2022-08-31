564e0c0a6cf06b93b84dc5190e6d6548753d71a1
Robin Karlsson <robin.karlsson@csc.fi>
2022-08-31 14:04:13 +0300

    Clean up csc_slurm_project

--- attributes/csc_slurm_project.rb
+++ attributes/csc_slurm_project.rb
@@ -34,17 +34,26 @@ module SmartAttributes
         (opts[:label] || "Project").to_s
       end
 
+      # returns an array with data-option-for (dynamic form JS) that hides the project when it
+      # can't be used by the currently selected reservation
+      # e.g. project_1234 is not on the list of accounts that can access the myreservation reservation:
+      # [{:"data-option-for-csc-slurm-reservation-myreservation": false}]
+      def reservations_data_option_for(project)
+        @reservations ||= SlurmReservation.reservations
+        data_option_for = @reservations.filter_map do |res|
+          res_name = res.name.gsub(/_/, '-')
+          allowed = (res.accounts + res.groups).uniq
+          {"data-option-for-csc-slurm-reservation-#{res_name}".to_sym => false} if !allowed.empty? && !allowed.include?(project)
+        end
+      end
+
       def select_choices
         @@select_choices ||=
           begin
-            reservations = SlurmReservation.reservations
             get_projects.collect do |p|
+              # Append description in parenthesis if not same as project name
               full_name = p[:description] == p[:name] ? p[:name] : "#{p[:name]} (#{p[:description]})"
-              data_option_for = reservations.filter_map do |res|
-                res_name = res.name.gsub(/_/, '-')
-                allowed = (res.accounts + res.groups).uniq
-                {"data-option-for-csc-slurm-reservation-#{res_name}".to_sym => false} if !allowed.empty? && !allowed.include?(p[:name])
-              end
+              data_option_for = reservations_data_option_for(p[:name])
               [full_name, p[:name], *data_option_for]
             end
           end
